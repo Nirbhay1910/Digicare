74401889a0f12dff4f78f5e8ed7da97b26b0a926
Nirbhay1910 <nirbhay1910@gmail.com>
2021-05-06 20:57:04 +0530

    added covidHelpingHands

--- src/components/CovidHelpingHands.js
+++ src/components/CovidHelpingHands.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './css/covidHelplingHands.css';
+import vaccine_img from '../images/vaccine_img.png';
+import advisories_img from '../images/advisories_image.png';
+import consult_doc_img from '../images/consult_doctor_img.png';
 
 function CovidHelpingHands() {
   return (
-    <div>
-      <h1>Covid-19 Helping hands</h1>
+    <div className='covidHelpingHands'>
+      <div className='covid_title'>
+        <h1>Covid-19 Helping Hands</h1>
+      </div>
+      <div className='covid-19_box'>
+        <div className='advisories'>
+          <h2>Advisories</h2>
+          <img src={advisories_img} alt='advisories img' />
+        </div>
+        <div className='advisories'>
+          <h2>Vaccine</h2>
+          <img src={vaccine_img} alt='vaccine img' />
+        </div>
+        <div className='advisories'>
+          <h2>Consult Doctor</h2>
+          <img src={consult_doc_img} alt='consult doc img' />
+        </div>
+      </div>
     </div>
   );
 }
