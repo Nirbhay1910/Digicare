b1cee021e4f2c4f60b94839307f618893eab6ad3
Nirbhay1910 <nirbhay1910@gmail.com>
2021-05-07 02:58:28 +0530

    added navbar and routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 import './css/navbar.css';
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar'>
+      <div className='logo'>
+        <img src={logo} alt='logo' />
+
+        <div className='logo-title'>
+          <h1>DIGICARE</h1>
+        </div>
+      </div>
+      <div className='navs'>
+        <div className='nav'>
+          <Link to='/'>
+            <h2 className='nav-option'>Home</h2>
+          </Link>
+          <h2 className='nav-option'>Doctor Consultation</h2>
+          <Link to='/talkWithStrangers'>
+            <h2 className='nav-option'>Talk with Others</h2>
+          </Link>
+          <h2 className='nav-option'>SignUp</h2>
+          <h2 className='nav-option'>Login</h2>
+        </div>
+      </div>
     </div>
   );
 }
