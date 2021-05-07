c7d8d5248d03947bf2d195b6b242f05b85b63d80
codewithmaddy <71321288+codewithmaddy@users.noreply.github.com>
2021-05-07 14:02:17 +0530

    about section done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './css/aboutUs.css';
+import about_img from '../images/AboutUs.png';
 
 function AboutUs() {
   return (
-    <div>
-      <h1>About our platform</h1>
+    <div className="about">
+      <div className="left">
+        <img src={about_img}></img>
+      </div>
+      <div className="right">
+        <div className="right-main">
+          <p>About The Platform</p>
+        </div>
+        <div className="right-sub">
+          <p>
+            Digicare is the all in one solution for people who can't consult to the doctor due to the panedemic with very minimal or no fees at all. 
+          </p>
+          <p>
+            We know these are very tough times. So, we also have mental care section where people can talk with others with similar interests so that everyone has someone to talk to.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
